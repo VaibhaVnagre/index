2e68a9f28a9e2340940b47f9951a6eeb66a4d15c
VaibhaVnagre <nagrevaibhav001@gmail.com>
2023-01-20 16:50:24 +0530

    dom1st_js

---
+++ dom1st.js
@@ -0,0 +1,24 @@
+//EXMAMIN THE DOCUMENT OBJECT
+//console.dir(document);
+//console.log(document.getElementById("header-title"));
+// const boomguys = document.getElementById("header-title");
+// //boomguys.textContent = "hello"
+// console.log(boomguys);
+// boomguys.innerText = "DIVINE"
+
+//const divine = document.getElementById("header-title");
+//console.log(divine.innerText);
+//divine.textContent="hello";
+
+ var color = document.getElementsByClassName("title");
+ color[0].style.color = 'green'
+
+var header = document.getElementById("main-header");
+header.style.borderBottom = "solid 3px black";
+
+const boom = document.getElementsByClassName("list-group-item");
+boom[1].style.fontWeight = "bold";
+boom[1].style.background = "yellow";
+
+// const botton = document.getElementsByClassName("btn btn-dark");
+// botton[0].style.backgroundColor = "orange";
